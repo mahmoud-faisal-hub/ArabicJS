b522b7ff4cae59036d036b79d487de39e946596e
Mahmoud Faisal <mahmoudfesal8@gmail.com>
2023-03-30 21:44:53 +0200

    chore(release.config.js): added package-lock.json to the assets of @semantic-release/git

--- release.config.js
+++ release.config.js
@@ -14,7 +14,7 @@ const config = {
     }],
     "@semantic-release/release-notes-generator",
     ["@semantic-release/git", {
-      "assets": ["dist/**/*.{js,ts}", "dist/**/*.{js,ts}.map", "docs", "package.json"],
+      "assets": ["dist/**/*.{js,ts}", "dist/**/*.{js,ts}.map", "docs", "package.json", "package-lock.json"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
     "@semantic-release/github"
