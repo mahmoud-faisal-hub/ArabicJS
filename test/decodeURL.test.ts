eb0c7aa09667c22c95b1f34a63bdb6f5f7bebc1e
Mahmoud Faisal <mahmoudfesal8@gmail.com>
2023-03-31 20:54:13 +0200

    test(decodeurl.test.ts): added test for `fixURL` function

--- test/decodeURL.test.ts
+++ test/decodeURL.test.ts
@@ -1,9 +1,10 @@
 import { test, expect } from 'vitest';
-import { decodeURL } from '../src';
+import { decodeURL, fixURL } from '../src';
 import ArabicJS from '../src';
 
 test('convert english date to arabic', () => {
   expect(decodeURL("https://ar.wikipedia.org/wiki/%D8%A7%D9%84%D8%B5%D9%81%D8%AD%D8%A9_%D8%A7%D9%84%D8%B1%D8%A6%D9%8A%D8%B3%D9%8A%D8%A9")).toBe("https://ar.wikipedia.org/wiki/الصفحة_الرئيسية");
+  expect(fixURL("https://ar.wikipedia.org/wiki/%D8%A7%D9%84%D8%B5%D9%81%D8%AD%D8%A9_%D8%A7%D9%84%D8%B1%D8%A6%D9%8A%D8%B3%D9%8A%D8%A9")).toBe("https://ar.wikipedia.org/wiki/الصفحة_الرئيسية");
   expect(ArabicJS("https://ar.wikipedia.org/wiki/%D8%A7%D9%84%D8%B5%D9%81%D8%AD%D8%A9_%D8%A7%D9%84%D8%B1%D8%A6%D9%8A%D8%B3%D9%8A%D8%A9").decodeURL().toString()).toBe("https://ar.wikipedia.org/wiki/الصفحة_الرئيسية");
   expect(ArabicJS("https://ar.wikipedia.org/wiki/%D8%A7%D9%84%D8%B5%D9%81%D8%AD%D8%A9_%D8%A7%D9%84%D8%B1%D8%A6%D9%8A%D8%B3%D9%8A%D8%A9").fixURL().toString()).toBe("https://ar.wikipedia.org/wiki/الصفحة_الرئيسية");
 });
