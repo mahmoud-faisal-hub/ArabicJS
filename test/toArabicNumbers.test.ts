b626770b57f292d8e7c51a945be554d8ae59b56c
Mahmoud Faisal <mahmoudfesal8@gmail.com>
2023-03-31 03:38:32 +0200

    test(toarabicnumbers.test.ts): add tests for `toArabicNumbers` function to check if accepts numbers

--- test/toArabicNumbers.test.ts
+++ test/toArabicNumbers.test.ts
@@ -4,5 +4,7 @@ import ArabicJS from '../src/index';
 
 test('convert english numbers to arabic', () => {
   expect(toArabicNumbers("تاريخ اليوم 30/3/2023")).toBe("تاريخ اليوم ٣٠/٣/٢٠٢٣");
+  expect(toArabicNumbers(30)).toBe("٣٠");
   expect(ArabicJS("تاريخ اليوم 30/3/2023").toArabicNumbers().toString()).toBe("تاريخ اليوم ٣٠/٣/٢٠٢٣");
+  expect(ArabicJS(30).toArabicNumbers().toString()).toBe("٣٠");
 });
